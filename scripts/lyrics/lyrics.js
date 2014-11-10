94087ef8905046cea1518b7b327b40bc5b05820c
Cory Reed <swashcap@gmail.com>
2014-11-09 16:56:10 -0800

    Add basic styles.

--- scripts/lyrics/lyrics.js
+++ scripts/lyrics/lyrics.js
@@ -7,20 +7,28 @@
 
       var lyrics = [{
         id: 0,
-        title: 'Lorem',
+        title: 'Proin vestibulum nunc in metus cursus at aliquam neque mollis.',
         trackNumber: 1
       }, {
         id: 1,
-        title: 'Ipsum',
+        title: ' Curabitur hendrerit porta nibh, a dignissim diam tempor eu. Nam id erat dictum velit facilisis tincidunt. Etiam auctor, felis et auctor blandit, eros quam aliquam orci, ac mollis neque dui vitae arcu.',
         trackNumber: 10
       }, {
         id: 2,
-        title: 'Dolor',
+        title: 'Fusce fringilla lectus ut ipsum condimentum a egestas odio pretium. Fusce vehicula erat a eros fermentum vestibulum. In a nisi eu dolor adipiscing facilisis sed et est. Suspendisse potenti.',
         trackNumber: 4
       }, {
         id: 3,
-        title: 'Sit Amet',
+        title: 'Praesent nec nulla elit, sit amet molestie metus.',
         trackNumber: 10
+      }, {
+        id: 4,
+        title: 'Curabitur consectetur, ipsum sit amet fringilla imperdiet, nisi nulla pharetra nunc, et consectetur nulla nisl a dolor.',
+        trackNumber: 4
+      }, {
+        id: 5,
+        title:  'Phasellus suscipit ipsum nec purus luctus ut euismod nisi dapibus. Vestibulum eget metus in lorem dignissim condimentum mattis non lorem.',
+        trackNumber: 4
       }];
 
       this.index = function () {
